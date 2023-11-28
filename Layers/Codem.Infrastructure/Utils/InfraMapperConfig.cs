91a4f8867c1ecc1e1f136b58b191809976f3b1a7
BaggerFast <riosha3@gmail.com>
2023-11-28 11:04:35 +0300

    FIX: UserId mapping

--- Layers/Codem.Infrastructure/Utils/InfraMapperConfig.cs
+++ Layers/Codem.Infrastructure/Utils/InfraMapperConfig.cs
@@ -37,16 +37,16 @@ public class InfraMapperConfig : IRegister
 
         config.ForType<SqlSnippetEntity, Snippet>()
             .Ignore(dest => dest.Password!)
+            .Ignore(dest => dest.UserId!)
             .Map(dest => dest.Id, src => src.Id)
             .Map(dest => dest.Title, src => src.Title)
             .Map(dest => dest.Visibility, src => src.Visibility)
             .Map(dest => dest.UserId, src => src.UserId)
             .Map(dest => dest.CreateDate, src => src.CreateDt)
-            .AfterMapping((src, dest) =>
-            {
+            .AfterMapping((src, dest) => {
                 dest.Password = !string.IsNullOrEmpty(src.Password) ? new Password(src.Password) : null;
+                dest.UserId = src.UserSnippetFk?.UserId;
             })
-            .Map(dest => dest.Files, src => src.Files.Adapt<List<SnippetFile>>())
-            .Map(dest => dest.UserId, src => src.UserSnippetFk != null ? src.UserSnippetFk.UserId : null);
+            .Map(dest => dest.Files, src => src.Files.Adapt<List<SnippetFile>>());
     }
 }

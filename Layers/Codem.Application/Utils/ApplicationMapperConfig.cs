039a789ab5d1f8f0366ea6e67b34467061f25b7e
BaggerFast <riosha3@gmail.com>
2023-11-29 21:04:59 +0300

    UPDATE: SnippetDto -> Snippet, FileDto -> File fix mappings

--- Layers/Codem.Application/Utils/ApplicationMapperConfig.cs
+++ Layers/Codem.Application/Utils/ApplicationMapperConfig.cs
@@ -1,6 +1,7 @@
 using Codem.Domain.Aggregates.SnippetAggregate;
 using Codem.Domain.ValueTypes;
 using Mapster;
+using Сodem.Shared.Dtos.File;
 
 namespace Codem.Application.Utils;
 
@@ -9,21 +10,33 @@ public class ApplicationMapperConfig : IRegister
 {
     public void Register(TypeAdapterConfig config)
     {
+        #region SnippetDto, Snippet
+
         config.ForType<SnippetDto, Snippet>()
             .Ignore(dest=>dest.Password!)
+            .Ignore(dest=>dest.CreateDate)
             .Map(dest => dest.Id, src => src.Id)
             .Map(dest => dest.Title, src => src.Title)
             .Map(dest => dest.Visibility, src=>src.Visibility)
             .Map(dest => dest.UserId, src => src.UserId)
-            .Map(dest => dest.CreateDate, src => src.CreateDate)
             .AfterMapping((src, dest) => 
             {
                 dest.Password = !string.IsNullOrEmpty(src.Password) ? new Password(src.Password) : null;
             })
             .Map(dest => dest.Files, src => src.Files.Adapt<IEnumerable<SnippetFile>>());
         
+        config.ForType<Snippet, SnippetDto>()
+            .Map(dest => dest.Id, src => src.Id)
+            .Map(dest => dest.Title, src => src.Title)
+            .Map(dest => dest.Visibility, src=>src.Visibility)
+            .Map(dest => dest.UserId, src => src.UserId)
+            .Map(dest => dest.Password, src => src.Password)
+            .Map(dest => dest.CreateDate, src => src.CreateDate)
+            .Map(dest => dest.Files, src => src.Files.Adapt<IEnumerable<FileDto>>());
+        
         config.ForType<SnippetCreateDto, Snippet>()
             .Ignore(dest => dest.Password!)
+            .Ignore(dest => dest.CreateDate)
             .Map(dest => dest.Title, src => src.Title)
             .Map(dest => dest.Visibility, src=>src.Visibility)
             .Map(dest => dest.UserId, src => src.UserId)
@@ -33,5 +46,28 @@ public class ApplicationMapperConfig : IRegister
                 dest.Password = !string.IsNullOrEmpty(src.Password) ? new Password(src.Password) : null;
             })
             .Map(dest => dest.Files, src => src.Files.Adapt<IEnumerable<SnippetFile>>());
+
+        #endregion
+        
+        #region FileDto, File
+
+        config.ForType<FileDto, SnippetFile>()
+            .Map(dest => dest.Id, src => src.Id)
+            .Map(dest => dest.ProgrammingLanguage, src => src.ProgrammingLanguage)
+            .Map(dest => dest.Data, src => src.Data)
+            .Map(dest => dest.Name, src => src.Name);
+        
+        config.ForType<SnippetFile, FileDto>()
+            .Map(dest => dest.Id, src => src.Id)
+            .Map(dest => dest.ProgrammingLanguage, src => src.ProgrammingLanguage)
+            .Map(dest => dest.Data, src => src.Data)
+            .Map(dest => dest.Name, src => src.Name);
+        
+        config.ForType<FileCreateDto, SnippetFile>()
+            .Map(dest => dest.ProgrammingLanguage, src => src.ProgrammingLanguage)
+            .Map(dest => dest.Data, src => src.Data)
+            .Map(dest => dest.Name, src => src.Name);
+
+        #endregion
     }
 }
